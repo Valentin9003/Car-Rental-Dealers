46a25621156617f60bfb111d794ddbf9e80e31ad
Valentin <Valio_180@abv.bg>
2020-06-11 06:27:09 +0300

    Add Car Ads Specification

---
+++ CarRentalSystem/Domain/Specifications/CarAds/CadAdOnlyAvailableSpecification.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq.Expressions;
+using Domain.Models.CarAds;
+
+namespace Domain.Specifications.CarAds
+{
+    public class CadAdOnlyAvailableSpecification : Specification<CarAd>
+    {
+        private readonly bool onlyAvailable;
+
+        public CadAdOnlyAvailableSpecification(bool onlyAvailable) 
+            => this.onlyAvailable = onlyAvailable;
+
+        public override Expression<Func<CarAd, bool>> ToExpression()
+        {
+            if (this.onlyAvailable)
+            {
+                return carAd => carAd.IsAvailable;
+            }
+
+            return carAd => true;
+        }
+    }
+}
---
+++ CarRentalSystem/Domain/Specifications/CarAds/CarAdByCategorySpecification.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+using Domain.Models.CarAds;
+
+namespace Domain.Specifications.CarAds
+{
+    public class CarAdByCategorySpecification : Specification<CarAd>
+    {
+        private readonly int? category;
+
+        public CarAdByCategorySpecification(int? category) 
+            => this.category = category;
+
+        protected override bool Include => this.category != null;
+
+        public override Expression<Func<CarAd, bool>> ToExpression()
+            => carAd => carAd.Category.Id == this.category;
+    }
+}
