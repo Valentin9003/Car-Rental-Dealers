af8eb05e6eec03e6435dca050e529f0341119c19
Valentin <Valio_180@abv.bg>
2020-06-12 06:55:03 +0300

    Add Application Common

---
+++ CarRentalSystem/Application/Common/SortOrder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Application.Common
+{
+    public abstract class SortOrder<TEntity>
+    {
+
+        public const string Ascending = "asc";
+        public const string Descending = "desc";
+
+        protected SortOrder(string? sortBy, string? order)
+        {
+            this.SortBy = sortBy;
+            this.Order = order;
+        }
+
+        public string? SortBy { get; }
+
+        public string? Order { get; }
+
+        public abstract Expression<Func<TEntity, object>> ToExpression();
+    }
+}
