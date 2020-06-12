12400c3d2ed1bb40539dc6615cbaf9e6516cccd5
Valentin <Valio_180@abv.bg>
2020-06-13 02:35:57 +0300

    Added Web Layer

---
+++ CarRentalSystem/Web/WebConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Application.Common;
+using Application.Contracts;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Web.Services;
+
+namespace Web
+{
+    public static class WebConfiguration
+    {
+        public static IServiceCollection AddWebComponents(this IServiceCollection services)
+        {
+            services
+                .AddScoped<ICurrentUser, CurrentUserService>()
+                .AddControllers()
+                .AddFluentValidation(validation => validation
+                    .RegisterValidatorsFromAssemblyContaining<Result>())
+                .AddNewtonsoftJson();
+
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+
+            return services;
+        }
+    }
+}
