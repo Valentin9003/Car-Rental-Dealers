a16eb5d18994a459f9d6233cf03e738b5d355988
Valentin <Valio_180@abv.bg>
2020-06-13 02:43:37 +0300

    no message

---
+++ CarRentalSystem/Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder
+                .HasOne(u => u.Dealer)
+                .WithOne()
+                .HasForeignKey<User>("DealerId")
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
