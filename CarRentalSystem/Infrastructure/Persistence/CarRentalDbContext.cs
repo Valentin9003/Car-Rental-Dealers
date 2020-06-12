6ee04167ee051ffb29060a0106d9e55f75ea3695
Valentin <Valio_180@abv.bg>
2020-06-12 09:04:02 +0300

    Add Persistence logic

---
+++ CarRentalSystem/Infrastructure/Persistence/CarRentalDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using Domain.Models.CarAds;
+using Domain.Models.Dealers;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    internal class CarRentalDbContext : IdentityDbContext<User>
+    {
+        public CarRentalDbContext(DbContextOptions<CarRentalDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CarAd> CarAds { get; set; } = default!;
+
+        public DbSet<Category> Categories { get; set; } = default!;
+
+        public DbSet<Manufacturer> Manufacturers { get; set; } = default!;
+
+        public DbSet<Dealer> Dealers { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
