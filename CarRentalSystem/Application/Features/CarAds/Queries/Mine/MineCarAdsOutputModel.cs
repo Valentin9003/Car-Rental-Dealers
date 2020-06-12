2084d1fd9ccdc1e7054323476560309cb1f10b34
Valentin <Valio_180@abv.bg>
2020-06-12 07:02:00 +0300

    Add CarAd Queries and Commands

---
+++ CarRentalSystem/Application/Features/CarAds/Queries/Mine/MineCarAdsOutputModel.cs
@@ -0,0 +1,16 @@
+﻿using Application.Features.CarAds.Queries.Common;
+using System.Collections.Generic;
+
+namespace Application.Features.CarAds.Queries.Mine
+{
+    public class MineCarAdsOutputModel : CarAdsOutputModel<MineCarAdOutputModel>
+    {
+        public MineCarAdsOutputModel(
+            IEnumerable<MineCarAdOutputModel> carAds, 
+            int page, 
+            int totalPages) 
+            : base(carAds, page, totalPages)
+        {
+        }
+    }
+}
