2c8fab607149427cd8432902e80fb1ee5fea96e6
Valentin <Valio_180@abv.bg>
2020-06-13 02:31:16 +0300

    Code Refactoring

--- CarRentalSystem/StartUp/Program.cs
+++ CarRentalSystem/StartUp/Program.cs
@@ -11,13 +11,11 @@ namespace StartUp
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+            => Host
+                .CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
